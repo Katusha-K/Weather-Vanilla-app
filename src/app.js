6ec57a9c9b5e3d28bf0aee7ef90a587be0a46fcf
Katusha-K <119417500+Katusha-K@users.noreply.github.com>
2022-12-13 12:50:35 +0100

    created a js file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  console.log(response.data.temperature.current);
+  let temperatuteElement = document.querySelector("#temperature");
+  temperatuteElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+}
 
+let apiKey = "b3fd63364t03b40b59o6a5657ae079db";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=Amsterdam&key=b3fd63364t03b40b59o6a5657ae079db&units=metric";
+
+axios.get(apiUrl).then(displayTemperature);
