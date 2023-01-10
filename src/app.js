40d1c9dd22b1321b3708890535a2447413914504
Katusha-K <119417500+Katusha-K@users.noreply.github.com>
2023-01-10 18:51:02 +0100

    Display a 5 days forecast, using an API

--- src/app.js
+++ src/app.js
@@ -20,29 +20,44 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
+  let forecast = response.data.daily;
   console.log(response.data.daily);
+
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Thu", "Fri", "Sat", "Sun"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHTML = `<div class="row">`;
 
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
-      <div class="weather-forecast-date">${day}</div>
+  forecast.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+      <div class="weather-forecast-date">${formatDay(day.time)}</div>
       <img
-        src="https://ssl.gstatic.com/onebox/weather/48/rain_s_cloudy.png"
-        alt=""
-        width="36"
-      />
+        src=${day.condition.icon_url}
+              />
       <div class="weather-forecast-temperatures">
-        <span class="weather-forecast-temperature-max">°14</span>
-        <span class="weather-forecast-temperature-min">°10</span>
+        <span class="weather-forecast-temperature-max">${Math.round(
+          day.temperature.maximum
+        )}°</span>
+        <span class="weather-forecast-temperature-min">${Math.round(
+          day.temperature.minimum
+        )}°</span>
       </div>
     </div>
   `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
